9d708241bc20a998dbb6a65db9793273b08839a3
Lucian Cancescu <lucian.cancescu@gmail.com>
2015-02-16 07:28:45 +0100

    sample radio app part 1

--- SampleRadioApp/ViewController.swift
+++ SampleRadioApp/ViewController.swift
@@ -7,12 +7,17 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var playButton: UIButton!
+    var player:AVPlayer = AVPlayer(URL: NSURL(string: "http://www.radiobrasov.ro/listen.m3u"))
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        playButton.setTitle("Play", forState: UIControlState.Normal)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +25,27 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func buttonPressed(sender: AnyObject) {
+        toggle()
+    }
+
+    func toggle() {
+        if playButton.titleLabel?.text == "Play" {
+            playRadio()
+        } else {
+            pauseRadio()
+        }
+    }
+    
+    func playRadio() {
+        player.play()
+        playButton.setTitle("Pause", forState: UIControlState.Normal)
+    }
+    
+    func pauseRadio() {
+        player.pause()
+        playButton.setTitle("Play", forState: UIControlState.Normal)
+    }
 
 }
 
